2f6d2b9dfe1818c1470cfe092ee4a2bdc812c0dd
daoming <daoming388@gmail.com>
2025-09-05 10:09:16 +0800

    cho5

---
+++ cho5/5.3_alien color_1and2.py
@@ -0,0 +1,16 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player scored 5 points\n")
+alien_color = 'red'
+if alien_color == 'green':
+    print("The player scored 5 points\n\n")
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("The player scored 5 points\n")
+else:
+    print("The player scored 10 points\n\n")
+    alien_color = 'green'
+if alien_color == 'green':
+    print("The player scored 5 points")
+else:
+    print("The player scored 10 points")
---
+++ cho5/5.7_favorite_fruits.py
@@ -0,0 +1,11 @@
+favorite_fruits =['Apple', 'Banana', 'Strawberry']
+if 'Apple' in favorite_fruits:
+    print("you are really like Apple!")
+if 'Banana' in favorite_fruits:
+    print("you are really like Banana!")
+if 'Strawberry' in favorite_fruits:
+    print("you are really like Strawberry!")
+if 'pears' in favorite_fruits:
+    print("you are really like pears!")
+if 'grapes' in favorite_fruits:
+    print("you are really like grapes!")          
