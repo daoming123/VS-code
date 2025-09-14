932d90c0c4b96a12df4d22e5be81e97e7e12f9a7
daoming <daoming388@gmail.com>
2025-09-14 19:00:39 +0800

    ch07

--- ch08/8.12_sandwich.py
+++
@@ -1,9 +0,0 @@
-def make_sandwich(*toppings):
-    """概述要制作的三明治"""
-    print("\nMaking a sandwich with the following toppings:")
-    for topping in toppings:
-        print(f"-{topping}")
-
-make_sandwich('pepperoni')
-make_sandwich('mushrooms', 'green peppers', 'extra cheese')
-make_sandwich('mushrooms', 'green peppers')
--- ch08/8.15_printing_ functions.py
+++
@@ -1,4 +0,0 @@
-import printing_models
-
-printing_models.make_pizza(16, 'pepperoni')
-printing_models.make_pizza(18, 'mushrooms')
