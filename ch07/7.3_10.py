1aa2494f13c998c75038881cb72d1e4a265aba2d
daoming123 <daoming388@gmail.com>
2025-09-27 12:30:57 +0800

    Delete ch07 directory

--- ch07/7.3_10.py
+++
@@ -1,6 +0,0 @@
-number = input("Enter a number, and I'll tell you if it's evenMultiples of 10 or no. ")
-number = int(number)
-if number % 10 == 0:
-    print(f"The number {number} is evenMultiples of 10.")
-else:
-    print(f"\nThe number {number} is no evenMultiples of 10.")
