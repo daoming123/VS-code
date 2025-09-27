6f74d3828e469bcebb5c81b9c725e2cf54276b30
daoming123 <daoming388@gmail.com>
2025-09-27 12:54:44 +0800

    ch05

---
+++ ch05/5.8and5.9_Greet the administrator.py
@@ -0,0 +1,10 @@
+"""Usernames."""
+usernames = ['mary', 'admin', 'jaden', 'sara', 'luna']
+if usernames:
+    for user in usernames:
+        if user == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
+else:
+    print("we need to find some user!")
