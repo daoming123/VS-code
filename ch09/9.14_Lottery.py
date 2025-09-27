ddb9f76865614855e9dbead36ef2c8d652b156f5
daoming123 <daoming388@gmail.com>
2025-09-27 12:57:05 +0800

    ch09

---
+++ ch09/9.14_Lottery.py
@@ -0,0 +1,13 @@
+import random
+
+lottery_pool = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,'a', 'b', 'c', 'd', 'e']
+winning_ticket = random.sample(lottery_pool, 4)
+print(f"你的彩票号码是{winning_ticket}")
+print("本期彩票号码是：24bf")
+print("只要彩票上有这 4 个数/字母，你就中奖了！")
+
+    
+
+
+
+
---
+++ ch09/test.py
@@ -0,0 +1,3 @@
+def add(x,y):return x+y
+z= add ( 1 ,2 )
+print(z)
