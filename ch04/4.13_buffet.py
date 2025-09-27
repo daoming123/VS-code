9931ec670b3781ced6c3e90fd20a74f3ccc4699c
daoming123 <daoming388@gmail.com>
2025-09-27 12:54:16 +0800

    ch04

---
+++ ch04/4.13_buffet.py
@@ -0,0 +1,7 @@
+foods = ("Fried Rice", "Noodles", "Salad", "Soup", "Sandwich")
+for food in foods:
+    print(food)
+
+foods = ("\nmango rice", "milk tea", "Salad", "Soup", "Sandwich")
+for food in foods:
+    print(food)
---
+++ ch04/4.2_animal.py
@@ -0,0 +1,6 @@
+"""Animals."""
+animals = ['eagle', 'sparrow', 'bat']
+for animal in animals:
+    print(animal)
+    print(f"A {animal.title()} would make a great pet.\n")
+print("Any of these animals would make a great pet!")
